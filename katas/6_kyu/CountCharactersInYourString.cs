8cd2b69029eccbfd51a9f8535a276258aa26689f
vmisi20 <vmisi20@gmail.com>
2021-01-31 22:00:28 +0100

    New Katas solved

---
+++ katas/6_kyu/CountCharactersInYourString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWarsKatas.katas._6_kyu
+{
+    // https://www.codewars.com/kata/52efefcbcdf57161d4000091/csharp
+    public class CountCharactersInYourString
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> countedLetters = new Dictionary<char, int>();
+
+            foreach (var letter in str)
+            {
+                if (countedLetters.ContainsKey(letter))
+                {
+                    countedLetters[letter] += 1;
+                }
+                else
+                {
+                    countedLetters.Add(letter, 1);
+                }
+            }
+
+            return countedLetters;
+        }
+    }
+}
---
+++ tests/CountCharactersInYourStringTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CodeWarsKatas.katas._6_kyu;
+using NUnit.Framework;
+
+namespace CodeWarsKatas.tests
+{
+    public class CountCharactersInYourStringTest
+    {
+        [Test]
+        public static void FixedTest_aaaa()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 4);
+            Assert.AreEqual(d, CountCharactersInYourString.Count("aaaa"));
+        }
+  
+        [Test]
+        public static void FixedTest_aabb()
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('a', 2);
+            d.Add('b', 2);
+            Assert.AreEqual(d, CountCharactersInYourString.Count("aabb"));
+        }
+    }
+}
